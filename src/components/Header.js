80f4b63a53b30c895b7e3526f25f877b22165e9d
deepak7691 <kawliya11@gmail.com>
2023-01-24 16:43:12 +0530

    Deepak

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,10 @@
+import React from "react";
 
+const {id , content} = props;
+return(
+    <div id="header">
+        <header id={id}>{content}</header> 
+    </div>
+);
+
+export default Header
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,1 +1,10 @@
+import React from "react";
 
+const {id , content} = props;
+return(
+    <div id="main">
+        <main id={id}>{content}</main> 
+    </div>
+);
+
+export default Main
