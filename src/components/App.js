f28d2d613401a5c93bdb590c8e8bf7211cb60c91
deepak7691 <119676343+deepak7691@users.noreply.github.com>
2023-01-25 00:42:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Main from "./Main";
 import Footer from "./Footer";
 function App() {
     return (
-        <div className="container">
+        <div id="container">
             <header id="my-header" content="This is the header"/>
             <main id="my-main" content="This is the main"/>
             <footer id="my-footer" content="This is the footer"/>
